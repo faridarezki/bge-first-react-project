8316b49186efcd4698594cecc089c81a398e9f37
Olivier Mangin <olivier@make-it.fr>
2020-04-01 11:19:39 +0200

    clean react app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Link from './Link';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Link attribute="Learn React !" />
-      </header>
+    <div>
     </div>
   );
 }
