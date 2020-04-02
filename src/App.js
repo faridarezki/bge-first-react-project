1e212898c3441af27e18c591c8d2945c37ff46ff
Olivier Mangin <olivier@make-it.fr>
2020-04-02 17:04:54 +0200

    add Navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
 
 import Home          from './pages/Home';
 import CreateArticle from './pages/CreateArticle';
@@ -13,6 +15,23 @@ import NotFound      from './pages/NotFound';
 const App = () => {
   return (
     <Router>
+      <Nav>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/">
+            Accueil
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/articles/create">
+            Créer un article
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link as={Link} to="/articles/delete">
+            Supprimer un article
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
       <Switch>
         <Route exact path="/"          component={Home}          />
         <Route path="/articles/create" component={CreateArticle} />
