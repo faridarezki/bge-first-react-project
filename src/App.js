29e5a8d444e9bcef32fd16ffc92b398f8824031e
Olivier Mangin <olivier@make-it.fr>
2020-04-03 10:52:13 +0200

    create component Navigation

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
+
+import Navigation from './components/Navigation';
 
 import Home          from './pages/Home';
 import CreateArticle from './pages/CreateArticle';
@@ -15,23 +15,7 @@ import NotFound      from './pages/NotFound';
 const App = () => {
   return (
     <Router>
-      <Nav>
-        <Nav.Item>
-          <Nav.Link as={Link} to="/">
-            Accueil
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link as={Link} to="/articles/create">
-            Créer un article
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link as={Link} to="/articles/delete">
-            Supprimer un article
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+      <Navigation />
       <Switch>
         <Route exact path="/"          component={Home}          />
         <Route path="/articles/create" component={CreateArticle} />
---
+++ src/components/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+
+const Navigation = () => {
+    return (
+        <Nav>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/">
+                    Accueil
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/articles/create">
+                    Créer un article
+                </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/articles/delete">
+                    Supprimer un article
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
+    );
+}
+
+export default Navigation;
