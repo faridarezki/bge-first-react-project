47bb1d26aa6206faf6fa1836422e2bdabbd64f1c
Olivier Mangin <olivier@make-it.fr>
2020-04-07 17:01:32 +0200

    add Card for article rendering

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Card      from 'react-bootstrap/Card';
 
 const Home = () => {
+    const [ articles, setArticles ] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api/articles')
+        .then((result) => {
+            return result.json();
+        })
+        .then(({ status, articles }) => {
+            if (status === "OK") {
+                setArticles(articles);
+            } else {
+                console.log("error : ", status);
+            }
+        })
+        .catch((error) => {
+            console.log("error : ", error);
+        });
+    }, []);
+
+    const renderedArticles = articles.map((article) => {
+        const date = new Date(article.created_at);
+        return (
+            <Card>
+                <Card.Header>
+                    <Card.Title as="h5">
+                        {article.title}
+                    </Card.Title>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        {article.content}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">
+                        créé le&nbsp;
+                        {date.getDate()}/{date.getMonth()+1}/{date.getFullYear()}&nbsp;
+                        par&nbsp;{article.authorFirstname} {article.authorLastname.substring(0, 1)}.
+                    </small>
+                </Card.Footer>
+            </Card>
+        );
+    });
+
     return (
-        <div>
+        <Container>
             <h1>Page d'accueil</h1>
-        </div>
+            <h2>Derniers articles</h2>
+            <CardGroup>
+                {renderedArticles}
+            </CardGroup>
+        </Container>
     );
 };
 
